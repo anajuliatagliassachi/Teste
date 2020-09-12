4d226b92324035a5dceb886e280373c7d1c91d86
naju <anajulia-tag@uol.com.br>
2020-09-12 11:58:50 -0300

    primeiro codigo

---
+++ teste.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+
+char linha1[256];
+char linha2[256];
+
+FILE *fd;
+
+fd = fopen("arq.txt","r");
+
+if (fd==NULL){ 
+  printf("Não foi possivel abrir o arquivo.\n"); 
+  exit(1); 
+}
+
+fgets(linha1, 255, fd);
+fgets(linha2, 255, fd);
+
+int entrada;
+
+scanf("%d",&entrada);
+if (entrada == 1){
+     printf("%s", linha1);
+  } else {
+    if (entrada == 2) {
+       printf("%s", linha2);
+      }
+  else{
+      printf("Entrada Invalida\n");
+  }
+  }
+
+
+}
